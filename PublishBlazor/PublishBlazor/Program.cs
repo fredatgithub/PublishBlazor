b8f89ac364d5d97f65cad5b02decf27b5a2fdb46
Fred <domaine75@gmail.com>
2020-11-07 13:00:51 +0100

    Initial commit

---
+++ PublishBlazor/PublishBlazor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublishBlazor
+{
+  class Program
+  {
+    static void Main(string[] arguments)
+    {
+      Action<string> display = Console.WriteLine;
+      display("Publication d'une application blazor");
+
+      display("Press any key to exit:");
+      Console.ReadKey();
+    }
+  }
+}
